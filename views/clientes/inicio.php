1b74629073d06c04a86b05f6d43caa3aa1f993a8
Itsjesus98 <jesus.granados.v99@gmail.com>
2022-11-23 22:26:04 -0600

    f.1

--- views/clientes/inicio.php
+++ views/clientes/inicio.php
@@ -17,7 +17,7 @@
      ?>
     
     <div class="card text-white bg-primary mb-3 mt-5 mx-auto col-lg-3" style="min-width:500px">
-  <div class="card-header"><?= $respuestan['titulo']?> <div class="name">Name: <?= $respuestan['name']?></div></div>
+  <div class="card-header"><?= $respuestan['titulo']?> <div class="fecha">Fecha: <?= $respuestan['fecha']?></div></div>
   <div class="card-body">
     <h4 class="card-title">Tipo: <?= $respuestan['perfil']?> </h4>
     <h4 class="card-title">De: <?= $respuestan['emisor']?> </h4>
@@ -31,12 +31,12 @@
       </select>
 </div>
          <?php $i++;}} } if($i==0){?>
-        <h4 class="center" style="color:red;">No tiene Carreras pendientes</h4>
+        <h4 class="center" style="color:red;">No tiene recomendaciones pendientes</h4>
          <?php }?>
 </div>
     </div>
 <div class="container mx-auto" id="diario">
-    <h3>Todas las Carreras</h3>
+    <h3>Todas las Recomendaciones</h3>
     <div class="container row">
     <?php if(isset($respuesta)){
         $i=0;
@@ -45,7 +45,7 @@
      ?>
     
     <div class="card text-white bg-success mb-3 mt-5 mx-auto col-lg-3" style="min-width:500px">
-  <div class="card-header"><?= $respuestan['titulo']?> <div class="name">Name: <?= $respuestan['name']?></div></div>
+  <div class="card-header"><?= $respuestan['titulo']?> <div class="fecha">Fecha: <?= $respuestan['fecha']?></div></div>
   <div class="card-body">
     <h4 class="card-title">Tipo: <?= $respuestan['perfil']?> </h4>
     <h4 class="card-title">De: <?= $respuestan['emisor']?> </h4>
@@ -54,7 +54,7 @@
   
 </div>
          <?php $i++; }} } if($i==0){?>
-        <h4 class="center" style="color:red;">No tiene carreras pendientes</h4>
+        <h4 class="center" style="color:red;">No tiene recomendaciones pendientes</h4>
          <?php }
          
     }?>
